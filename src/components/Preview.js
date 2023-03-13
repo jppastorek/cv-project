1d27d50ab0d6b28632e8fd54023f0d1d4afc2bff
Jon Pastorek <jppastorek@gmail.com>
2023-03-12 21:46:18 -0500

    removed unnecessary comments

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-//this will be used to show a preview of your CV. it will have a button that will allow you to save as a PDF
-//each section will have an edit button that populates the form with those values to change
-
 const Preview = (props) => {
   const { first, last, title, email, tel, website, about, jobs, degrees } =
     props;
